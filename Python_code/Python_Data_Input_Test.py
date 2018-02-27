27863e8f69b55a3723b4e701fd9f8143809510f5
James Hutson <hutsonj@live.unc.edu>
2018-02-27 14:03:04 -0500

    thing

--- Python_code/Python_Data_Input_Test.py
+++ Python_code/Python_Data_Input_Test.py
@@ -2,6 +2,24 @@ import serial
 import numpy
 import matplotlib.pyplot as plt
 
+# arrays for number of licks
+cage1Licks = []
+cage2Licks = []
+cage3Licks = []
+
+# arrays for Time
+
+cage1Times = []
+cage2Times = []
+cage3Times = []
+
+def parseLicksAndTimes(cage, cageLicks, cageTimes):
+    singleLicksAndTime = cage.split(",")
+    licks = singleLicksAndTime[0]
+    time = singleLicksAndTime[1]
+    cageLicks.append(licks)
+    cageTimes.append(time)
+
 arduinoData = serial.Serial('com4', 9600)
 
 while True:
@@ -15,11 +33,11 @@ while True:
     cage2 = licksAndTimes[1]
     cage3 = licksAndTimes[2]
 
-    print cage1
+    parseLicksAndTimes(cage1, cage1Licks, cage1Times)
+    parseLicksAndTimes(cage2, cage2Licks, cage2Times)
+    parseLicksAndTimes(cage3, cage3Licks, cage3Times)
 
-def parseLicksAndTimes(cage):
-    singleLicksAndTime = cage.split(",")
-    return singleLicksAndTime
+
 
 
 
