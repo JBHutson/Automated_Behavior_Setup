a0d7f6da1e8e5045ce59a80eb2a12a7d943b75ca
James Hutson <hutsonjb@gmail.com>
2019-03-09 23:01:29 -0500

    refactor to pyqt5

--- Main_Code/Data_Acquisition_And_Visualization/Data_Input_And_Graphing.py
+++ Main_Code/Data_Acquisition_And_Visualization/Data_Input_And_Graphing.py
@@ -2,55 +2,26 @@
 
 import serial
 import time
-# import numpy
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
-# arrays for number of licks
-cage1Licks = []
-cage2Licks = []
-cage3Licks = []
+class dataModel:
 
-# arrays for Time
+    def __init__(self):
+        self.cage1Licks = []
+        self.cage2Licks = []
+        self.cage3Licks = []
+        self.cage4Licks = []
+        self.cage1Times = []
+        self.cage2Times = []
+        self.cage3Times = []
+        self.cage4Times = []
 
-cage1Times = []
-cage2Times = []
-cage3Times = []
-
-def parseLicksAndTimes(cage, cageLicks, cageTimes):
-    singleLicksAndTime = cage.split(",")
-    licks = int(singleLicksAndTime[0])
-    time = singleLicksAndTime[1]
-    cageLicks.append(licks)
-    cageTimes.append(time)
-    print licks
-
-def manipulation(dataLine):
-    licksAndTimes = dataLine.split(';')
-
-f = open('random.txt', 'r')
-
-while 1:
-    where = f.tell()
-    line = f.readline()
-    if not line:
-        time.sleep(1)
-        f.seek(where)
-    else:
-        manipulation(line)
-
-    # first parse
-    # cage1 = licksAndTimes[0]
-    # cage2 = licksAndTimes[1]
-    # cage3 = licksAndTimes[2]
-    
-
-    #parseLicksAndTimes(cage1, cage1Licks, cage1Times)
-   # parseLicksAndTimes(cage2, cage2Licks, cage2Times)
-   # parseLicksAndTimes(cage3, cage3Licks, cage3Times)
-
-
-
-
-
+    def parseLicksAndTimes(self, cage, cageLicks, cageTimes):
+        singleLicksAndTime = cage.split(",")
+        licks = int(singleLicksAndTime[0])
+        time = singleLicksAndTime[1]
+        cageLicks.append(licks)
+        cageTimes.append(time)
 
 
--- Main_Code/GUI/Main_GUI.py
+++ Main_Code/GUI/Main_GUI.py
@@ -1,9 +1,16 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+
 import sys
 
-class App(QWidget):
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+import random
+
+class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -16,13 +23,55 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.resize(self.width, self.height)
         self.center()
+        
+        self.mainWidget = QWidget(self)
+
+        windowLayout = QHBoxLayout(self.mainWidget)
+
+        cageOne = PlotCanvas(self.mainWidget)
+        cageOne.setName('Cage 1')
+        cageTwo = PlotCanvas(self.mainWidget)
+        cageTwo.setName('Cage 2')
+        cageThree = PlotCanvas(self.mainWidget)
+        cageThree.setName('Cage 3')
+        cageFour = PlotCanvas(self.mainWidget)
+        cageFour.setName('Cage 4')
+
+        windowLayout.addWidget(cageOne)
+        windowLayout.addWidget(cageTwo)
+        windowLayout.addWidget(cageThree)
+        windowLayout.addWidget(cageFour)
+
+        self.mainWidget.setFocus()
+        self.setCentralWidget(self.mainWidget)
+
+        self.show()
 
     def center(self):
         qtRectangle = self.frameGeometry()
         centerPoint = QDesktopWidget().availableGeometry().center()
         qtRectangle.moveCenter(centerPoint)
         self.move(qtRectangle.topLeft())
-        self.show()
+
+class PlotCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+
+        FigureCanvas.__init__(self, fig)
+        
+        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plot()
+
+    def plot(self):
+        data = [random.random() for i in range(25)]
+        self.axes.plot(data, 'r-')
+        self.draw()
+
+    def setName(self, plotName):
+        self.axes.set_title(plotName)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
