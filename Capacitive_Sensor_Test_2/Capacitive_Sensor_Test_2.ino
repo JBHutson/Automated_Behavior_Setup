f6c5d9580c19d01a7fed5da3e559b8305fc29bbf
JBHutson <hutsonjb@gmail.com>
2019-11-13 16:14:02 -0500

    started refactor

--- Capacitive_Sensor_Test_2/Capacitive_Sensor_Test_2.ino
+++ Capacitive_Sensor_Test_2/Capacitive_Sensor_Test_2.ino
@@ -1,18 +1,11 @@
 // arduino code that creates and runs multiple capacitive circut lick-o-meters simultaniously
 
 #include <CapacitiveSensor.h>
-// initiate globals variables for number of licks, number of loop cycles, two value holders and 
-// the capacitive sensor object itself 
+// initiate globals variables for number of licks, number of loop cycles, two value holders and
+// the capacitive sensor object itself
 int lick1, lick2, lick3;
-float curTime1 = 0;
-float curTime2 = 0;
-float curTime3 = 0;
-int hour1, hour2, hour3;
-int minute1, minute2, minute3; 
-int second1, second2, second3;
 int loopCount = 1;
 long cs1_1v, cs1_2v, cs2_1v, cs2_2v, cs3_1v, cs3_2v;
-String dateTime1, dateTime2, dateTime3;
 CapacitiveSensor cs1 = CapacitiveSensor(4, 5);
 CapacitiveSensor cs2 = CapacitiveSensor(2, 3);
 CapacitiveSensor cs3 = CapacitiveSensor(6, 7);
@@ -30,7 +23,6 @@ void loop() {
     // check to see if the current value is over threshold and previous value was under threshold
     // if so, the licks value is incremented by 1
     if (cs1_1v > 40 && cs1_2v < 80){
-      curTime1 = millis();
       lick1++;
     }
   } else {
@@ -38,72 +30,39 @@ void loop() {
     // if so, the licks value is incremented by 1
     cs1_2v = cs1.capacitiveSensor(80);
     if (cs1_2v > 40 && cs1_1v < 80){
-      curTime1 = millis();
       lick1++;
     }
   }
    if (loopCount % 2 != 0){
     cs2_1v = cs2.capacitiveSensor(80);
     if (cs2_1v > 40 && cs2_2v < 80){
-      curTime2 = millis();
       lick2++;
     }
   } else {
     cs2_2v = cs2.capacitiveSensor(80);
     if (cs2_2v > 1000 && cs2_1v < 1500){
-      curTime2 = millis();
       lick2++;
     }
   }
    if (loopCount % 2 != 0){
     cs3_1v = cs3.capacitiveSensor(80);
     if (cs3_1v > 1000 && cs3_2v < 1500){
-      curTime3 = millis();
       lick3++;
     }
   } else {
     cs3_2v = cs3.capacitiveSensor(80);
     if (cs3_2v > 1000 && cs3_1v < 1500){
-      curTime3 = millis();
       lick3++;
     }
   }
 
-  // take the milliseconds that the program has been running and transform into hour:min:sec
-  second1 = curTime1/1000;
-  second1 = round(second1);
-  minute1 = floor(second1/60);
-  second1 = second1%60;
-  hour1 = floor(minute1/60);
-  minute1 = minute1%60;
-  dateTime1 = String(hour1) + ":" + String(minute1) + ":" + String(second1);
-  second2 = curTime2/1000;
-  second2 = round(second2);
-  minute2 = floor(second2/60);
-  second2 = second2%60;
-  hour2 = floor(minute2/60);
-  minute2 = minute2%60;
-  dateTime2 = String(hour2) + ":" + String(minute2) + ":" + String(second2);
-  second3 = curTime3/1000;
-  second3 = round(second3);
-  minute3 = floor(second3/60);
-  second3 = second3%60;
-  hour3 = floor(minute3/60);
-  minute3 = minute3%60;
-  dateTime3 = String(hour3) + ":" + String(minute3) + ":" + String(second3);
-
   // print the lick and timestamp values to the serial port
   Serial.print(lick1);
-  Serial.print(" , ");
-  Serial.print(dateTime1);
-  Serial.print(" ; ");
+  Serial.print(":");
   Serial.print(lick2);
-  Serial.print(" , ");
-  Serial.print(dateTime2);
-  Serial.print(" ; ");
+  Serial.print(":");
   Serial.print(lick3);
-  Serial.print(" , ");
-  Serial.println(dateTime3);
+  Serial.print("\n");
   delay(20);
   loopCount++;
 }
--- main_code/data_acquisition/app.py
+++ main_code/data_acquisition/app.py
@@ -1,34 +1,37 @@
-#!flask/bin/python
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-mice = [
-    {
-        'id': 1,
-        'active': True
-    },
-    {
-        'id': 2,
-        'active': False
-    },
-    {
-        'id': 3,
-        'active': True
-    },
-    {
-        'id': 4,
-        'active': False
-    }      
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class Cage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    minute = db.Column(db.Integer, unique=False, nullable=False)
+    lick = db.Column(db.Integer, unique=False, nullable=False)
+    mouse = db.Column(db.String(80), unique=False, nullable=False)
+    time = db.Column(db.DateTime(), unique=False, nullable=False)
+    cage = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return self.mouse
+
+
+@app.route('/')
+def index():
+    return 'server running'
 
 @app.route('/mice_active', methods=['GET'])
 def get_mice_active():
-    i = 0
+    active_mice = []
     for mouse in mice:
         if mouse['active'] == True:
-            i = i+1
-    return str(i)
+            active_mice.append({'id': mouse['id']})
+    return jsonify(active_mice)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/cage_info/<mouse>', methods=['GET'])
+def get_data(mouse):
+    mouse_info = Cage.query.filter_by(mouse=mouse)
+    return jsonify(mouse_info)
