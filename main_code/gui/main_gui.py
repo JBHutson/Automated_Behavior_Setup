bdb6cd3cfc86b75431e51b77595e9b2fb60a8fd6
JBHutson <hutsonjb@gmail.com>
2019-11-14 18:04:02 -0500

    added animation

--- main_code/gui/main_gui.py
+++ main_code/gui/main_gui.py
@@ -2,13 +2,14 @@
 
 import random
 import sys
+import time
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 class App(QMainWindow):
 
@@ -17,30 +18,24 @@ class App(QMainWindow):
         self.title = 'Lick-o-Meter'
         self.width = 1200
         self.height = 900
+        self.mainWidget = QWidget(self)
+        self.cageOne = PlotCanvas(self.mainWidget)
+        self.cageTwo = PlotCanvas(self.mainWidget)
+        self.cageThree = PlotCanvas(self.mainWidget)
+        self.cageFour = PlotCanvas(self.mainWidget)
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.resize(self.width, self.height)
         self.center()
-        
-        self.mainWidget = QWidget(self)
 
         windowLayout = QGridLayout(self.mainWidget)
 
-        cageOne = PlotCanvas(self.mainWidget)
-        cageOne.setName('Cage 1')
-        cageTwo = PlotCanvas(self.mainWidget)
-        cageTwo.setName('Cage 2')
-        cageThree = PlotCanvas(self.mainWidget)
-        cageThree.setName('Cage 3')
-        cageFour = PlotCanvas(self.mainWidget)
-        cageFour.setName('Cage 4')
-
-        windowLayout.addWidget(cageOne,0,0)
-        windowLayout.addWidget(cageTwo,0,1)
-        windowLayout.addWidget(cageThree,1,0)
-        windowLayout.addWidget(cageFour,1,1)
+        windowLayout.addWidget(self.cageOne,0,0)
+        windowLayout.addWidget(self.cageTwo,0,1)
+        windowLayout.addWidget(self.cageThree,1,0)
+        windowLayout.addWidget(self.cageFour,1,1)
 
         self.mainWidget.setFocus()
         self.setCentralWidget(self.mainWidget)
@@ -56,21 +51,30 @@ class App(QMainWindow):
 class PlotCanvas(FigureCanvas):
 
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
 
-        FigureCanvas.__init__(self, fig)
-        
+        FigureCanvas.__init__(self, self.fig)
         FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
-        self.plot()
+        self.ax = self.fig.add_subplot(1, 1, 1)
+        self.fig.subplots_adjust(bottom=0.30)
+        self.xs = []
+        self.ys = []
+        self.i = 1
 
-    def plot(self):
-        self.axes.set_xticks([1,2,3,4,5,6,7,8,9,10], minor=True)
-        self.axes.set_yticks(range(1,51), minor=True)
-        self.axes.set_xlabel('Minute of Experiment')
-        self.axes.set_ylabel('Licks')
-        self.draw()
+    def animate(self, i):
+        self.xs.append(self.i)
+        self.i = self.i + 1
+        self.ys.append(random.randint(1, 21))
+
+        self.xs = self.xs[-10:]
+        self.ys = self.ys[-10:]
+
+        self.ax.clear()
+        self.ax.plot(self.xs, self.ys)
+
+        self.ax.set_xlabel('Minute of Experiment')
+        self.ax.set_ylabel('Licks')
 
     def setName(self, plotName):
         self.axes.set_title(plotName)
@@ -78,4 +82,8 @@ class PlotCanvas(FigureCanvas):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
+    ani = animation.FuncAnimation(ex.cageOne.fig, ex.cageOne.animate, interval=1000)
+    ani = animation.FuncAnimation(ex.cageTwo.fig, ex.cageTwo.animate, interval=1000)
+    ani = animation.FuncAnimation(ex.cageThree.fig, ex.cageThree.animate, interval=1000)
+    ani = animation.FuncAnimation(ex.cageFour.fig, ex.cageFour.animate, interval=1000)
     sys.exit(app.exec())
